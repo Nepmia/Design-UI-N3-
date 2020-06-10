c55c22ed669476079510f7d1544b68b344f88bb6
root <root@planepserver-r.pulseheberg.com>
2020-06-10 22:42:45 +0200

    Routing templates

--- dsui.py
+++ dsui.py
@@ -4,6 +4,7 @@ app = Flask(__name__,
             static_url_path='', 
             static_folder='N3/static',
             template_folder='N3/template')
+            
 @app.route('/')
 @app.route('/N3/')
 def dsuimain(name=None):
