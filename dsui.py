f76cb9e3f32818725b7838c7c35d52330bda9681
root <root@planepserver-r.pulseheberg.com>
2020-06-10 22:24:06 +0200

    N3 Workspace import + base preparation

---
+++ dsui.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='N3/static',
+            template_folder='N3/template')
+@app.route('/')
+@app.route('/N3/')
+def dsuimain(name=None):
+    return render_template('base.html', title= 'Test')
