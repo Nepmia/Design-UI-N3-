e7e3b2e26308fbdf644d172d96ffed1fe1ef33ef
root <root@planepserver-r.pulseheberg.com>
2020-06-14 15:29:07 +0200

    Added dynamic flex animation and resized the tiles

--- N3/static/js/main.js
+++ N3/static/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
           $('.navbar').addClass('primary-color').removeClass('transparent');
           $('#SPNGI').attr('width','35').attr('height','35');
       } else {
-          $('.navbar').removeClass('primary-color').addClass('tranparent');
+          $('.navbar').removeClass('primary-color').addClass('transparent');
           $('#SPNGI').attr('width','50').attr('height','50');
       }
     });
@@ -30,8 +30,81 @@ window.onload = function(){
 
 $(document).ready(function(){
     $('.dsui-img-container').hover(function(){
+    var $HT = 25;
+        if ( $(this).find('p').height() < $HT){
+
+        
         $(this).find('p').addClass('visibleDSH').removeClass('hiddenDSH');
-    }, function(){
-        $(this).find('p').removeClass('visibleDSH').addClass('hiddenDSH');
+    } else if ($(this).find('p').height() > $HT){
+        $(this).find('p').addClass('visibleDSHH').removeClass('hiddenDSH');
+    }}, function(){
+        $(this).find('p').removeClass('visibleDSH').addClass('hiddenDSH').removeClass('visibleDSHH');
     })
 })
+
+
+$(window).resize(function(){
+    var $minW = 500;
+    if ( $(window).width() < $minW) {
+        $('.dsui-subcontainer').css('justify-content','center');
+    }
+    else if ( $(window).width() > $minW) {
+        $('.dsui-subcontainer').css('justify-content','left');
+    }
+})
+
+window.addEventListener('load', function(event) {
+    var targetClassName = 'flex-wrap-anim';
+    var defaultDuration = '0.8s';
+  
+    var dummyList = [];
+    function addDummy(item, duration) {
+      var top = item.offsetTop;
+      var left = item.offsetLeft;
+      setTimeout(function() {
+        item.style.position = 'absolute';
+        item.style.top = top + 'px';
+        item.style.left = left + 'px';
+  
+        var dummyDiv = document.createElement('div');
+        dummyDiv.classList.add(targetClassName + '-dummy');
+        var rect = item.getBoundingClientRect();
+        dummyDiv.style.width = rect.width + 'px';
+        dummyDiv.style.height = rect.height + 'px';
+        dummyDiv.style.margin = '5px';
+        dummyDiv.style.visibility = 'hidden';
+        dummyDiv['__' + targetClassName + '_pair'] = item;
+        dummyDiv['__' + targetClassName + '_duration'] = duration;
+        item.parentNode.appendChild(dummyDiv);
+        dummyList.push(dummyDiv);
+      }, 0);
+    }
+  
+    var conts = document.getElementsByClassName(targetClassName);
+    for (var i = 0, max = conts.length; i < max; i++) {
+      var cont = conts[i];
+      cont.style.position = 'relative';
+      var duration = cont.getAttribute('data-duration')
+        || defaultDuration;
+      var items = cont.getElementsByTagName('div');
+      for (var i = 0, max = items.length; i < max; i++) {
+        addDummy(items[i], duration);
+      }
+    }
+  
+    window.addEventListener('resize', function(event) {
+      dummyList.forEach(function(dummyDiv) {
+        var item = dummyDiv['__' + targetClassName + '_pair'];
+        var duration = dummyDiv['__' + targetClassName + '_duration'];
+        if (item.offsetTop != dummyDiv.offsetTop) {
+          item.style.transition = 'all ' + duration;
+          item.style.top = dummyDiv.offsetTop + 'px';
+          item.style.left = dummyDiv.offsetLeft + 'px';
+        } else {
+          item.style.transition = '';
+          item.style.left = dummyDiv.offsetLeft + 'px';
+        }
+      });
+    });
+  });
+  
