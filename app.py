768da2f8574b960681db45519afdd37fede1de3a
root <root@planepserver-r.pulseheberg.com>
2020-06-17 21:45:34 +0200

    Added the all images directory

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ app = Flask(__name__,
 @app.route('/')
 @app.route('/N3/')
 def dsuimain(name=None):
+    all = os.listdir(os.path.join(app.static_folder, "all"))
     demilsez = os.listdir(os.path.join(app.static_folder, "demilsez"))
     demildiset = os.listdir(os.path.join(app.static_folder, "demildiset"))
     demildihouit = os.listdir(os.path.join(app.static_folder, "demildihouit"))
     demildineuf = os.listdir(os.path.join(app.static_folder, "demildineuf"))
     demilvin = os.listdir(os.path.join(app.static_folder, "demilvin"))
-    return render_template('base.html', demilsez=demilsez, demildiset=demildiset, demildihouit=demildihouit, demildineuf=demildineuf, demilvin=demilvin)
+    return render_template('base.html', demilsez=demilsez, demildiset=demildiset, demildihouit=demildihouit, demildineuf=demildineuf, demilvin=demilvin, all=all)
